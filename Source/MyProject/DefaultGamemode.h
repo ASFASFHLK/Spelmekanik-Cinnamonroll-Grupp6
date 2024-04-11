6a8886278d83863826a3a90e184b29421a433649
Lysandra <46670248+xenutrash@users.noreply.github.com>
2024-04-11 12:09:35 +0200

    added win/lose screens

--- Source/MyProject/DefaultGamemode.h
+++ Source/MyProject/DefaultGamemode.h
@@ -13,5 +13,13 @@ UCLASS()
 class MYPROJECT_API ADefaultGamemode : public AGameModeBase
 {
 	GENERATED_BODY()
+
+private:
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UUserWidget> WinScreen;
+
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UUserWidget> LoseScreen;
+
 	
 };

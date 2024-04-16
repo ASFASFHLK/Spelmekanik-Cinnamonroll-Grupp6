efcc56dba2defb277eac30603899b84abb16277f
Lysandra <46670248+xenutrash@users.noreply.github.com>
2024-04-16 12:38:57 +0200

    removed old useless files

--- Intermediate_old/Build/Win64/x64/MyProjectEditor/Development/Engine/SharedPCH.Engine.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Engine/Public/EngineSharedPCH.h
-#include "C:/Users/june/Documents/GitHub/Spelmekanik-Cinnamonroll-Grupp6/Intermediate/Build/Win64/x64/MyProjectEditor/Development/Engine/SharedDefinitions.Engine.Cpp20.h"
-#include "Runtime/Engine/Public/EngineSharedPCH.h"

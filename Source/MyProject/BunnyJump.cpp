2d0546bebd1d0551feda87e5c27d10649494bdfb
Lysandra <46670248+xenutrash@users.noreply.github.com>
2024-04-18 22:17:27 +0200

    Bunny jumping added (super scuffed)

---
+++ Source/MyProject/BunnyJump.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BunnyJump.h"
+
+#include "PlayerCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void ABunnyJump::OnAdded()
+{
+	Super::OnAdded();
+	MovementComponent = PlayerCharacter->GetCharacterMovement();
+	DefaultSpeed = MovementComponent->MaxWalkSpeed;
+	UE_LOG(LogTemp, Warning, TEXT("Default speed %f"), DefaultSpeed)
+}
+
+void ABunnyJump::OnUpdate(float DeltaTime)
+{
+	Super::OnUpdate(DeltaTime);
+
+	if(MovementComponent->IsFalling())
+	{
+		
+		if(CurrentSpeedInc >= MaxSpeed or SpeedAdded)
+		{
+			return;
+		}
+		SpeedAdded = true;
+		UE_LOG(LogTemp, Warning, TEXT("Adding speed"))
+		CurrentSpeedInc+= SpeedInc;
+		MovementComponent->MaxWalkSpeed+= SpeedInc;
+	}
+	else
+	{
+		Accumulator+= DeltaTime;
+		if(Accumulator > Timing)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Reseting speed"))
+			CurrentSpeedInc = 0;
+			Accumulator = 0;
+			MovementComponent->MaxWalkSpeed = DefaultSpeed;
+		}
+		SpeedAdded = false;
+	}
+	
+}
